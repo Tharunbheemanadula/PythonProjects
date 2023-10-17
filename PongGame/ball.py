e259de744c16177a3562f9e5fa31ced2684e99de
Bheemanadula Tharun <63277741+Tharunbheemanadula@users.noreply.github.com>
2023-10-17 15:43:47 +0530

    Add files via upload

---
+++ PongGame/ball.py
@@ -0,0 +1,35 @@
+import time
+from turtle import  Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x =10
+        self.y =10
+        #ballspeed
+        self.move_speed = 0.1
+
+
+
+    def move(self):
+        new_x=self.xcor()+self.x
+        new_y =  self.ycor()+self.y
+        self.goto(new_x,new_y)
+
+
+    def bounce_y(self):
+        self.y*=-1
+
+    def bounce_x(self):
+        self.x*=-1
+        self.move_speed*=0.9
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.move_speed=0.1
+        time.sleep(0.5)
+        self.bounce_x()
